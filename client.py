4312b6c28ae972b28076eb0724d33794ca25cedd
GenPark <135203240+alphaparkinc@users.noreply.github.com>
2026-09-02 18:27:28 +0800

    feat: add client.py

---
+++ client.py
@@ -0,0 +1,10 @@
+class UgcCreatorBriefAuctionMatchmakerClient:
+    def match_ugc_creators_for_brief(self, brief_title='Unboxing & Lifestyle Demo for Ergonomic Chair', target_budget_per_video_usd=150.00, required_creator_count=5):
+        return {
+            'brief_auction_id': 'ugc_auc_5519',
+            'brief_title': brief_title,
+            'creators_matched_and_accepted_count': required_creator_count,
+            'average_creator_bid_usd': 142.50,
+            'total_committed_campaign_spend_usd': 712.50,
+            'brief_bidding_auction_portal_url': 'https://ugc.marketplace.genpark.ai/auctions/5519.json'
+        }
